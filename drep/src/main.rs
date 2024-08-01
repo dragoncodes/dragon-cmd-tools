cec62282912e62109a7633049b52ba0c697ce956
petar.marinov <dragoncodeseu@gmail.com>
2024-08-01 22:52:55 +0300

    Fixed coloring for match at position 0

--- drep/src/main.rs
+++ drep/src/main.rs
@@ -9,21 +9,24 @@ fn main() {
         .read_to_string(&mut input)
         .expect("Error reading input");
 
-    let search_phrase = std::env::args().nth(1);
+    let original_search_phrase = std::env::args().nth(1);
 
-    let search_phrase = search_phrase.expect("Empty search phrase");
+    let original_search_phrase = original_search_phrase.expect("Empty search phrase");
 
-    let search_phrase = Regex::new(&search_phrase).unwrap();
+    let search_phrase = Regex::new(&original_search_phrase.to_lowercase()).unwrap();
 
     input.lines().for_each(|line| {
-        if let Some(m) = search_phrase.find(line) {
+        let line = line.to_lowercase().to_owned();
+
+        if let Some(m) = search_phrase.find(&line) {
             let match_start = m.start();
 
             if match_start == 0 {
-                let start = &line[..(match_start)];
-                let end = &line[(match_start)..];
+                let original_search_phrase_len = original_search_phrase.len();
+                let start = &line[0..original_search_phrase_len];
+                let end = &line[original_search_phrase_len..];
 
-                println!("{}{}", start.red(), end);
+                println!("{}{}", start.truecolor(255, 125, 121), end);
             } else {
                 println!(
                     "{}{}{}",
